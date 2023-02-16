924342c7878cd044615ac09ff20aeec66e8823db
Michael Thomas <mike.ahpong@gmail.com>
2023-02-16 13:46:44 +0800

    added productsDataService file to post requests to the springBoot backend

---
+++ CaltechAngularFrontEndFinalProject/src/app/service/data/product-data.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/list-product/list-product.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  retrieveAllTodos(username :any){
+    return this.httpClient.get<Product[]>(`http://localhost:8080/users/${username}/todos`)
+  }
+
+ retrieveTodo(username :any, id :number){
+    return this.httpClient.get<Product[]>(`http://localhost:8080/users/${username}/todos/${id}`)
+  }
+
+  updateTodo(username: any,id: number,todo: Product){
+    return this.httpClient.put(`http://localhost:8080/users/${username}/todos/${id}`,todo);
+  }
+
+  createTodo(username: any, todo: Product) {
+    return this.httpClient.post(`http://localhost:8080/users/${username}/todos`,todo);
+  }
+}

201bcba7d50fc59f522e3d93838568a958f7c79d
Michael Thomas <mike.ahpong@gmail.com>
2023-02-22 11:52:09 +0800

    updated routing module for users and user-details

--- CaltechAngularFrontEndFinalProject/src/app/app-routing.module.ts
+++ CaltechAngularFrontEndFinalProject/src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UserService } from './service/user/user.service';
+// import { PlaceholderComponent } from './../components/placeholder/placeholder.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 const routes: Routes = [
   // {path: '', component:LoginComponent},
   {path: 'login', component:LoginComponent},
@@ -11,12 +15,25 @@ const routes: Routes = [
 
 
   // {path: '**', component:ErrorComponent}
+  {
+    path: 'users',
+    component: UsersComponent,
+    // canActivateChild: [ ActivateGuard ],
+    /* resolve: {
+      users: UsersResolveGuard
+    }, */
+    children: [
+      { path: ':userId', component: UserDetailsComponent },
+      // { path: '', component: PlaceholderComponent }
+    ]
+  },
 
 ];
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [UserService ]
 })
 export class AppRoutingModule { }

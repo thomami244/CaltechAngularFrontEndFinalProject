76bb25af361aab9bf661451d2b9d777b854d6e10
Michael Thomas <mike.ahpong@gmail.com>
2023-02-22 14:28:21 +0800

    routing to /users and /shop works

--- CaltechAngularFrontEndFinalProject/src/app/app-routing.module.ts
+++ CaltechAngularFrontEndFinalProject/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserService } from './service/user/user.service';
 // import { PlaceholderComponent } from './../components/placeholder/placeholder.component';
+import { FoodMenuComponent } from './food-menu/food-menu.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 const routes: Routes = [
   // {path: '', component:LoginComponent},
+  { path: 'shop', component: FoodMenuComponent },
   {path: 'login', component:LoginComponent},
   // {path: 'welcome/:name', component:WelcomeComponent, canActivate:[RouteGuardService]},
   // {path: 'todos', component:ListTodosComponent, canActivate:[RouteGuardService]},

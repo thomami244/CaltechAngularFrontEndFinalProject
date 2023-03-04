240637ae32a09eecff47f751eaacbb0cf9caaabd
Michael Thomas <mike.ahpong@gmail.com>
2023-03-04 13:12:54 +0800

    clearing up list product component

--- CaltechAngularFrontEndFinalProject/src/app/list-product/list-product.component.ts
+++ CaltechAngularFrontEndFinalProject/src/app/list-product/list-product.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit, HostListener,NgModule, Injectable, Inject, Input, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Input, Output, NgModule, Injectable, Inject,  } from '@angular/core';
+import { ProductDataService } from '../service/data/product-data.service';
+import { FoodMenuComponent } from '../food-menu/food-menu.component';
 import { FormControl,  FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FoodMenuComponent } from '../food-menu/food-menu.component';
-import { LoginComponent } from '../login/login.component';
-import { ProductComponent } from '../product/product.component';
-import { ProductDataService } from '../service/data/product-data.service';
-import { Subscription } from 'rxjs';
 
-// import { TodoDataService } from '../service/data/todo-data.service';
+
+import { IProduct } from './../interfaces/product';
+
+import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-list-product',
+  templateUrl: './list-product.component.html',
+  styleUrls: ['./list-product.component.css'],
+  providers: [ProductDataService],
+})
 
 @NgModule({
   imports: [
@@ -22,40 +30,78 @@ import { Subscription } from 'rxjs';
 )
 
 
-@Injectable()
-export class Product {
-  activatedRoute!: ActivatedRoute,
+export class ListProductComponent implements OnInit {
 
-  constructor (
-    @Inject(String) public product_id : number,
-    @Inject(String)  public productname : string,
-    @Inject(String)  public productdescription : string,
-    @Inject(String)  public productURL : string,
-    @Inject(String)  public productcategory : string,
-    @Inject(String) public productprice : number,
-    @Inject(String) public availablequantity : number,
-    @Inject(String) public productquantity : number,
-    @Inject(Number) public producttotal : number,
-    @Inject(String) public isActivated: boolean
-    )
+  products: IProduct[] | undefined;
+  subscription!: Subscription;
+  product_id: number =0
+  quantity = new FormControl('');
+  productname: string =''
+  productdescription: string =''
+  productprice: number =0
+  availablequantity: number =0
 
-    {
+  @Input() category: string ="meals"
+
+  @Input() username!: string
+  @Input() password!: string
+  @Input() productquantity: number =0
+  @Output()productquantityChange: number =0
+
+  @Input() producttotal: number =0
+  @Output()producttotalChange: number =0
+
+
+  // @Input() adminLoggedIn:Boolean = false;
+  // param!: any;
+  // message: any;
+  // newData: any;
+
+
+  constructor(
+
+    private activatedRoute: ActivatedRoute,
+    private productdataservice: ProductDataService,
+
+  ) { }
+
+  ngOnInit() {
+
+
+
+    this.subscription = this.productdataservice.retrieveAllProducts().subscribe((products: Product[]) => {
+      this.products = products;
+      console.log(products);
+      console.log(products[0].product_id);
+      console.log(products[0].productname);
+      console.log(products[0].productdescription);
+      console.log(products[0].productURL);
+      console.log(products[0].productcategory);
+      console.log(products[0].productprice);
+      console.log(products[0].availablequantity);
+      console.log(products[0].productquantity);
+      console.log(products[0].producttotal);
+      console.log(products[0].isActivated);
+
+
+      console.log(products[1].product_id);
+      console.log(products[1].productname);
+      console.log(products[1].productdescription);
+    });
   }
-}
 
-@Component({
-  selector: 'app-list-product',
-  templateUrl: './list-product.component.html',
-  styleUrls: ['./list-product.component.css'],
-  providers: [LoginComponent, ProductDataService],
-})
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+}
+function getData() {
+  throw new Error('Function not implemented.');
+}
 
 
 export class ListProductComponent implements OnInit{
-  // ngOnInit() {
-  //   this.refreshProducts()
-  // }
-  // products: Product[] =[]
+
   products = [Product];
   product_id: number =0
   quantity = new FormControl('');
@@ -78,143 +124,31 @@ export class ListProductComponent implements OnInit{
     public foodmenucomponent: FoodMenuComponent,
     public logincomponent: LoginComponent,
     public productdataservice: ProductDataService,
+    private activatedRoute!: ActivatedRoute,
 
   ){}
 
 
   populateData(product_id: any, productprice: number, productname:any, productquantity: number){
 
-    // console.log(Product)
-    // console.log(this.quantity);
-    // console.log(this.products);
+
     console.log(product_id);
     console.log(productname);
     console.log(productquantity);
     this.producttotal = productquantity * productprice;
 
-    // this.producttotal =  (Math.round(this.producttotal* 100 )/100).toFixed(2);
+
 
     console.log(this.producttotal)
 
-    // this.product.setValue({
-    //   name: 'Akash',
-    //   email: 'akash@xyz.com',
-    //   gender: 'male',
-    //   paymentType: 'Yearly',
-    //   notification: 'false',
-    // });
+
   }
 
   ngOnInit(): void {
-    this.subscription = this.productdataservice.retrieveAllProducts().subscribe((products: Product[]) => {
-      this.products = products;
-      console.log(products);
-      console.log(products[0].product_id);
-      console.log(products[0].productname);
-      console.log(products[0].productdescription);
-      console.log(products[0].productURL);
-      console.log(products[0].productcategory);
-      console.log(products[0].productprice);
-      console.log(products[0].availablequantity);
-      console.log(products[0].productquantity);
-      console.log(products[0].producttotal);
-      console.log(products[0].isActivated);
 
-
-      console.log(products[1].product_id);
-      console.log(products[1].productname);
-      console.log(products[1].productdescription);
-    });
 
 
 
   }
 
-  // products: Product[] = [
-  //   {product_id:1,
-  //     productname: 'Coca-Cola',
-  //     productdescription: 'Fizzy Drink',
-  //     productURL: 'www.coca-cola.com',
-  //     productcategory: 'drinks',
-  //     productprice: 1.99,
-  //     availablequantity: 100,
-  //     availablequantity: 0,
-  //     producttotal: 0,
-  //     isActivated: true},
-
-  //   { product_id:2,
-  //     productname: 'burghers',
-  //     productdescription: 'description burghers',
-  //     productURL: 'www.burghers',
-  //     productcategory: 'meals',
-  //     productprice: 9.99,
-  //     availablequantity: 20,
-  //     productquantity: 0,
-  //     producttotal: 0,
-  //     isActivated: true},
-
-  //     { product_id:3,
-  //       productname: 'Potato wedges',
-  //       productdescription: 'potato wedges with great flavor',
-  //       productURL: 'www.potatowedges.com',
-  //       productcategory: 'sides',
-  //       productprice: 2.99,
-  //       availablequantity: 15,
-  //       productquantity: 0,
-  //       producttotal: 0,
-  //       isActivated: true},
-
-  //       { product_id:4,
-  //         productname: 'Seared fish',
-  //         productdescription: 'Oven-baked fish in olive oil',
-  //         productURL: 'www.fishnchipse.com',
-  //         productcategory: 'meals',
-  //         productprice: 15.99,
-  //         availablequantity: 9,
-  //         productquantity: 0,
-  //         producttotal: 0,
-  //         isActivated: true}
-  // ]
-
 }
-
-// updateItem(productname) {
-//   console.log(productname)
-//   // let url: string = "/update-item/" + name;
-//   // this.router.navigateByUrl(url);
-// }
-
-    // constructor(private todoDataService: TodoDataService, private router: Router) { }
-
-    // ngOnInit() {
-    //   this.refreshTodos()
-    // }
-  //   todo5: Todo = new Todo(1,``, false, new Date)
-
-
-  //   todos4: Todo[] = [];
-
-  // refreshTodos(){
-  //   this.todoDataService.retrieveAllTodos('vinodh').subscribe(
-  //     response => {
-  //       // console.log(response);
-  //       this.todos4 = response;
-  //     }
-  //   )
-  // }
-  // updateTodo(id: any){
-  //   // console.log(id);
-  //   this.router.navigate(['todos', id])
-
-  // }
-  //     todos3 = [
-  //   new Todo(1,'Learn to Dance', false, new Date() ),
-  //   new Todo(2,'Become an expert at angular', false, new Date() ),
-  //   new Todo(3,'visit the USA', false, new Date() )
-  //   ]
-
-
-
-// function updateItem(product_name: any, string: any) {
-//   throw new Error('Function not implemented.');
-// }
